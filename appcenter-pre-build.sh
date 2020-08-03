806bbdf23bdd2f38a3ad9582c6f9992593ce3499
Wanderlei B. Oliveira <wanderlei.brandao@gmail.com>
2020-08-03 00:56:46 -0300

    Appc
    Atualizando script appcenter

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,7 +2,7 @@ GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/android/app/google-services.json
 
 touch $GOOGLE_JSON_FILE
 
-if[ -e "GOOGLE_JSON_FILE"]
+if [ -e "GOOGLE_JSON_FILE" ]
 then 
       echo "Updating Google Json"
       echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
